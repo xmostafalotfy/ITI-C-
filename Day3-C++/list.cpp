b13962b5c21c974f9ec37c18357e0500efbe6620
Mostafa Lotfy <mostafamohammmed82@gmail.com>
2024-12-05 23:08:24 +0200

    some updates

--- Day3-C++/list.cpp
+++ Day3-C++/list.cpp
@@ -2,89 +2,119 @@
 #include <stdexcept>
 using namespace std;
 
-class list
-{
-    int *items;
+class list {
+    int* items;
     int size;
 
 public:
     static int listsNum;
 
-    list()
-    {
+    list() {
         this->size = 0;
         items = nullptr;
         listsNum++;
     }
 
-    list(const list &l)
-    {
-        this->size = l.size;
+    list(int arr[],int arrSize) {
+        this->size = arrSize;
 
-        if (size > 0)
-        {
+        if (size > 0) {
             this->items = new int[size];
 
-            for (int i = 0; i < size; i++)
-            {
-                this->items[i] = l.items[i];
+            for (int i = 0; i < size; i++) {
+                this->items[i] = arr[i];
             }
-        }
-        else
+        } else
             this->items = nullptr;
-        
+
         listsNum++;
     }
 
-    list &operator=(const list &l)
-    {
+
+    list(const list& l) {
+        this->size = l.size;
+
+        if (size > 0) {
+            this->items = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                this->items[i] = l.items[i];
+            }
+        } else
+            this->items = nullptr;
+
+        listsNum++;
+    }
+
+    list& operator=(const list& l) {
         if (this == &l)
             return *this;
 
         delete[] items;
         this->size = l.size;
 
-        if (size > 0)
-        {
+        if (size > 0) {
             this->items = new int[size];
-            for (int i = 0; i < size; i++)
-            {
+            for (int i = 0; i < size; i++) {
                 this->items[i] = l.items[i];
             }
-        }
-        else
+        } else
             this->items = nullptr;
 
         return *this;
     }
 
-    bool operator==(const list &l) const
-    {
-        if (this->size != l.size)
-            return false;
-
-        for (int i = 0; i < size; i++)
-        {
-            if (this->items[i] != l.items[i])
-                return false;
+    char operator==( list& l){
+        if (this->size == l.size){
+            for(int i = 0; i < size; i++){
+                if (this->items[i]!=l.items[i])
+                    return 0;
+            }
+            return 1;
         }
-
-        return true;
+        return 0;
     }
 
-    void insert(int index, int data)
-    {
-        if (index < 0 || index > size)
-        {
+    char operator<(const list& l)  {
+        if (this->size < l.size)
+            return 1;
+        else
+            return 0;
+    }
+
+    bool operator>(const list& l)  {
+        if (this->size > l.size)
+            return 1;
+        else
+            return 0;
+    }
+
+    list slice(int startIndex, int endIndex, int counter)  {
+        if(endIndex == -1)
+        endIndex = this->size;
+
+        if (startIndex < 0 || endIndex >= this->size || startIndex > endIndex) {
+            throw "Error: out of index range";
+        }
+        list temp ;
+        for(int i = startIndex; i < endIndex; i+=counter){
+            temp.items[i] = this->items[i];
+        }
+        temp.size = endIndex - startIndex;
+
+        return temp;
+    }
+
+    void insert(int index, int data) {
+        if (index < 0 || index > size) {
             throw "Wrong Index...!";
         }
 
-        int *newItems = new int[size + 1];
-        for (int i = 0; i < size + 1; i++)
-        {
+        int* newItems = new int[size + 1];
+        for (int i = 0; i < size + 1; i++) {
             if (i == index)
                 newItems[i] = data;
-            else if(i > index)
+            else if (i > index)
                 newItems[i] = items[i - 1];
             else
                 newItems[i] = items[i];
@@ -95,11 +125,9 @@ public:
         size++;
     }
 
-    void push(int data)
-    {
-        int *newItems = new int[size + 1];
-        for (int i = 0; i < size; i++)
-        {
+    void push(int data) {
+        int* newItems = new int[size + 1];
+        for (int i = 0; i < size; i++) {
             newItems[i] = items[i];
         }
         newItems[size] = data;
@@ -109,14 +137,12 @@ public:
         size++;
     }
 
-    int pop()
-    {
+    int pop() {
         if (size == 0)
             throw "Stack is Empty";
 
-        int *newItems = new int[size - 1];
-        for (int i = 0; i < size - 1; i++)
-        {
+        int* newItems = new int[size - 1];
+        for (int i = 0; i < size - 1; i++) {
             newItems[i] = items[i];
         }
         int value = items[size - 1];
@@ -127,17 +153,14 @@ public:
         return value;
     }
 
-    int remove(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+    int remove(int index) {
+        if (index < 0 || index >= size) {
             throw "Index out of range";
         }
 
         int x = items[index];
-        int *newItems = new int[size - 1];
-        for (int i = 0; i < size - 1; i++)
-        {
+        int* newItems = new int[size - 1];
+        for (int i = 0; i < size - 1; i++) {
             if (i >= index)
                 newItems[i] = items[i + 1];
             else
@@ -150,24 +173,21 @@ public:
         return x;
     }
 
-    void display()
-    {
+    void display() const {
         if (size == 0)
-            cout << "Stack is empty." << endl;
+            cout << "List is empty." << endl;
 
-        for (int i = size - 1; i >= 0; i--)
-        {
-            cout << items[i] << "\t";
+        for (int i = 0; i < size; i++) {
+            cout << items[i] << " ";
         }
+        cout << endl;
     }
 
-    int len()
-    {
+    int len()  {
         return size;
     }
 
-    ~list()
-    {
+    ~list() {
         delete[] items;
         listsNum--;
     }
@@ -175,7 +195,7 @@ public:
 
 int list::listsNum = 0;
 
-int main()
-{
+int main() {
+
     return 0;
 }
--- Day3-C++/string.cpp
+++ Day3-C++/string.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 using namespace std;
 /*
 
@@ -15,146 +16,188 @@ cin cout << >> overload
 class String
 {
 private:
-
     char *ptr = nullptr;
     int size = 0;
 
 public:
+    friend ostream &operator<<(ostream &out, const String &st);
 
-friend ostream& operator<<(ostream& out, const String& st);
+    friend istream &operator>>(istream &in, String &st);
 
-friend istream& operator>>(istream& in, String& st);
-
-
-
-    String(){
-
+    String()
+    {
     }
 
-    String(const char *st){
+    String(const char *st)
+    {
         int counter = 0;
 
-        while (st[counter]!='\0'){
+        while (st[counter] != '\0')
+        {
             counter++;
         }
 
         size = counter;
-        ptr = new char[size+1];
+        ptr = new char[size + 1];
 
-        for (int i = 0;i<size;i++){
+        for (int i = 0; i < size; i++)
+        {
             ptr[i] = st[i];
         }
         ptr[size] = '\0';
     }
 
-    void operator=(const char *st) {
+    void operator=(const char *st)
+    {
         int counter = 0;
 
-        while (st[counter]!='\0'){
+        while (st[counter] != '\0')
+        {
             counter++;
         }
 
         size = counter;
-        ptr = new char[size+1];
+        ptr = new char[size + 1];
 
-        for (int i = 0;i<size;i++){
+        for (int i = 0; i < size; i++)
+        {
             ptr[i] = st[i];
         }
         ptr[size] = '\0';
     }
 
-    String& operator=(const String &other) {
+    String &operator=(String &other)
+    {
 
-    if (this != &other) {
+        if (this != &other)
+        {
 
-        delete[] ptr;
-        size = other.size;
-        ptr = new char[size];
+            delete[] ptr;
+            size = other.size;
+            ptr = new char[size];
 
-        for (int i = 0; i < size; i++) {
-            ptr[i] = other.ptr[i];
+            for (int i = 0; i < size; i++)
+            {
+                ptr[i] = other.ptr[i];
+            }
         }
-    }
-    return *this;
+        return *this;
     }
 
-    String operator+(const String &other)const{
+    String operator+(String &other)
+    {
         String result;
         int newSize = size + other.size;
         result.ptr = new char[newSize];
         result.size = newSize;
-        for(int i = 0; i<size;i++){
+        for (int i = 0; i < size; i++)
+        {
             result.ptr[i] = ptr[i];
         }
-        for(int i =0;i<other.size;i++){
-            result.ptr[size+i] = other.ptr[i];
+        for (int i = 0; i < other.size; i++)
+        {
+            result.ptr[size + i] = other.ptr[i];
         }
         result.ptr[newSize] = '\0';
         return result;
     }
 
-    int len(){
+    int len()
+    {
         return size;
     }
 
-    char operator[](int index) {
-    return ptr[index];
+    char operator[](int index)
+    {
+        if (index < size && index >= 0)
+            return ptr[index];
+        else
+            throw "Out of index range";
+    }
+
+    String& slice(int startIndex=0,int endIndex=-1,int counter=1) {
+        if(endIndex == -1)
+        endIndex = this->size;
+        if (startIndex < 0 || endIndex >= this->size || startIndex > endIndex) {
+            throw "Error: out of index range";
+        }
+        String temp ;
+        for(int i = startIndex; i < endIndex; i+=counter){
+            temp.ptr[i] = this->ptr[i];
+        }
+        temp.size = endIndex - startIndex;
+        temp.ptr[temp.size] = '\0';
+
+        return temp;
+    }
+
+    char operator<(String &s){
+        if(s.len()<this->len())
+        return 0;
+        else
+        return 1;
+    }
+
+    char operator>(String &s){
+        if(s.len()>this->len())
+        return 0;
+        else
+        return 1;
+    }
+
+    char operator==(String &s){
+        if (this->size == s.size){
+            for(int i = 0; i < size; i++){
+                if (this->ptr[i]!=s.ptr[i])
+                    return 0;
+            }
+            return 1;
+        }
+        return 0;
     }
 
 
-    ~String(){
+    ~String()
+    {
         delete[] ptr;
     }
 };
 
-ostream& operator<<(ostream& out, const String& st)
+ostream &operator<<(ostream &out, const String &st)
 {
-    for(int i = 0;i<st.size;i++){
+    for (int i = 0; i < st.size; i++)
+    {
         out << st.ptr[i];
     }
     return out;
 };
 
-istream& operator>>(istream& in, String& st){
+istream &operator>>(istream &in, String &st)
+{
 
-    char buffer[1000]; 
+    char buffer[1000];
     in >> buffer;
 
     delete[] st.ptr;
     int length = 0;
-    while (buffer[length] != '\0') {
+    while (buffer[length] != '\0')
+    {
         length++;
     }
 
     st.ptr = new char[length + 1];
     st.size = length;
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         st.ptr[i] = buffer[i];
     }
     st.ptr[length] = '\0';
 
     return in;
-
 };
 
+int main() {   
 
-int main(){
-    // String x("test");
-    // cout<<x.len();
-    // cout<<x;
-    // cout<<x[1];
-    // String y("no");
-    // cout<<endl;
-    // x=y;
-    // cout<<x;
-    String x,y;
-    // x = "123";
-    // y = "456";
-    // y = x+y;
-    // cout<<y<<endl;
-    // cout<<x<<endl;
-    cin >> x;
-    cout<<"\n"<<x<<endl;
     return 0;
 }
